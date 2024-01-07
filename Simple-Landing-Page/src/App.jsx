1c03efbc73588839be90ff4547e23aa5c8c3831c
Ayushkt02 <ayushkumartiwary_it22a8_73@dtu.ac.in>
2024-01-07 16:36:47 +0530

    Nav Ready

--- Simple-Landing-Page/src/App.jsx
+++ Simple-Landing-Page/src/App.jsx
@@ -1,20 +1,10 @@
 import "./App.css"
+import Navigation from "./componenets/Navigation";
 
 const App = () => {
   return (
     <div>
-      <nav>
-        <div>
-          <img src="https://imgs.search.brave.com/twMmfYD8_om9nFWkvPjZWubfUg1D2FC9R4HmNeqfPJo/rs:fit:860:0:0/g:ce/aHR0cHM6Ly8xMDAw/bG9nb3MubmV0L3dw/LWNvbnRlbnQvdXBs/b2Fkcy8yMDIxLzEx/L05pa2UtTG9nby01/MDB4MjgxLnBuZw" alt="logo" />
-        </div>
-        <ul>
-          <li href="#">Menu</li>
-          <li href="#">Location</li>
-          <li href="#">About</li>
-          <li href="#">Contact</li>
-        </ul>
-        <button>LogIn</button>
-      </nav>
+      <Navigation/>
     </div>
   )
 };
--- Simple-Landing-Page/src/main.jsx
+++ Simple-Landing-Page/src/main.jsx
@@ -7,7 +7,6 @@ import App from './App'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <App /> */}
-    Lorem, ipsum dolor.
     <App/>
   </React.StrictMode>,
 )
